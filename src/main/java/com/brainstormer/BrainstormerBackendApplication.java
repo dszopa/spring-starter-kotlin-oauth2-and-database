12af93410bd303d1b553fe630938602fe79e144e
Daniel Szopa <szopanator@gmail.com>
2017-06-08 00:24:22 -0500

    Initial Commit - Setup basic spring project - Looking into security on a rest webservice

---
+++ src/main/java/com/brainstormer/BrainstormerBackendApplication.java
@@ -0,0 +1,12 @@
+package com.brainstormer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainstormerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainstormerBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/brainstormer/BrainstormerBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.brainstormer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BrainstormerBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
