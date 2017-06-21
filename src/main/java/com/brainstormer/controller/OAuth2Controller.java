37de6392566bbdbbd2fe629de17813575847132e
Daniel Szopa <szopanator@gmail.com>
2017-06-20 19:46:59 -0500

    Implement logout

---
+++ src/main/java/com/brainstormer/controller/OAuth2Controller.java
@@ -0,0 +1,36 @@
+package com.brainstormer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@PreAuthorize("isFullyAuthenticated()")
+public class OAuth2Controller {
+
+    private TokenStore tokenStore;
+
+    @Autowired
+    public OAuth2Controller(TokenStore tokenStore) {
+        this.tokenStore = tokenStore;
+    }
+
+    @RequestMapping(value = "/oauth/revoke-token", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public void logout(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null) {
+            String tokenValue = authHeader.replace("Bearer", "").trim();
+            OAuth2AccessToken accessToken = tokenStore.readAccessToken(tokenValue);
+            tokenStore.removeAccessToken(accessToken);
+        }
+    }
+}
