4e89631af53231ec702abc2bc33365156cfd3c7f
Daniel Szopa <szopanator@gmail.com>
2017-06-20 20:28:25 -0500

    Use built in Spring OAuth2 Services for logout - Rather than manipulating the database directly (shouldn't happen in the controller layer)

--- src/main/java/com/brainstormer/controller/OAuth2Controller.java
+++ src/main/java/com/brainstormer/controller/OAuth2Controller.java
@@ -4,33 +4,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
-import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
+import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.security.Principal;
 
 @RestController
 @PreAuthorize("isFullyAuthenticated()")
 public class OAuth2Controller {
 
-    private TokenStore tokenStore;
+    private AuthorizationServerTokenServices authorizationServerTokenServices;
+    private ConsumerTokenServices consumerTokenServices;
 
     @Autowired
-    public OAuth2Controller(TokenStore tokenStore) {
-        this.tokenStore = tokenStore;
+    public OAuth2Controller(AuthorizationServerTokenServices authorizationServerTokenServices, ConsumerTokenServices consumerTokenServices) {
+        this.authorizationServerTokenServices = authorizationServerTokenServices;
+        this.consumerTokenServices = consumerTokenServices;
     }
 
     @RequestMapping(value = "/oauth/revoke-token", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
-    public void logout(HttpServletRequest request) {
-        String authHeader = request.getHeader("Authorization");
-        if (authHeader != null) {
-            String tokenValue = authHeader.replace("Bearer", "").trim();
-            OAuth2AccessToken accessToken = tokenStore.readAccessToken(tokenValue);
-            tokenStore.removeAccessToken(accessToken);
-        }
+    public void logout(Principal principal) throws IOException {
+        OAuth2Authentication oAuth2Authentication = (OAuth2Authentication) principal;
+        OAuth2AccessToken accessToken = authorizationServerTokenServices.getAccessToken(oAuth2Authentication);
+        consumerTokenServices.revokeToken(accessToken.getValue());
     }
 }
